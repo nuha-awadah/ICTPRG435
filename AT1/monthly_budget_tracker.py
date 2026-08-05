e8fda1cb7526e103df8d81a60dc868be37997d9c
Nuha Awadah <nuhaawadah24@gmail.com>
2026-08-05 23:13:34 +0800

    added try catch input validation test

--- AT1/monthly_budget_tracker.py
+++ AT1/monthly_budget_tracker.py
@@ -1,35 +1,14 @@
-# Fixed Pseudocode:
-# 1.	BEGIN
-# 2.	OUTPUT “Please enter your monthly income”
-# 3.	INPUT wagePerMonth
-# 4.	WHILE wagePerMonth < 0 OR wagePerMonth == isNumeric
-    # a.	OUTPUT “Your wage is in the negatives or is not numeric. Please re-enter your wage”
-    # b.	INPUT wagePerMonth
-# 5.	ENDWHILE
-# 6.	SET totalExpenses = 0
-# 7.	OUTPUT “Please enter your expense category and amounts”
-# 8.	INPUT expenseCategory
-# 9.	INPUT expenseAmount
-# 10.	OUTPUT “Do you want to continue (Y/N)?”
-# 11.	INPUT continueButton
-# 12.	WHILE continueButton = “Y”
-    # a.	INPUT expenseCategory
-    # b.	totalExpenseCategory = totalExpenseCategory + expenseCategory
-    # c.	INPUT expenseAmount
-    # d.	totalExpenseAmount =  totalExpenseAmount + expenseAmount
-    # e.	OUTPUT “Do you want to continue (Y/N)?”
-    # f.	INPUT continueButton
-# 13.	ENDWHILE
-# 14.	remainingBalance = wagePerMonth - totalExpenseAmount
-# 15.	OUTPUT wagePerMonth,  totalExpenseAmount, remainingBalance
-# 16.	END
-
-wagePerMonth = float(input("Enter your wage per month: "))
-
-# implement a try catch input validation test here
-# 4.	WHILE wagePerMonth < 0 OR wagePerMonth == isNumeric
-    # a.	OUTPUT “Your wage is in the negatives or is not numeric. Please re-enter your wage”
-    # b.	INPUT wagePerMonth
+# input validation while loop + try catch
+while True:
+    try:
+        wagePerMonth = float(input("Enter your wage per hour: "))
+        if wagePerMonth <= 0:
+            print("Please enter a positive number")
+        else:
+            break
+    except ValueError:
+        # handles input error
+        print("Please enter a numeric value")
 
 expenseCategory = input("Enter your expense category: ")
 expenseCategory = expenseCategory.upper()
