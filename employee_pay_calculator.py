4aaf37747a96cde32776850a1afd586e4afbb3b8
Nuha Awadah <nuhaawadah24@gmail.com>
2026-08-05 20:53:46 +0800

    deleted pseudocode, added a few comments for clarity in employee_pay_calculator.py. Added pseudocode for monthly_budget_tracker.py

--- employee_pay_calculator.py
+++ employee_pay_calculator.py
@@ -3,11 +3,14 @@ numberHourWork = float(input(f"How many hours work?\n"))
 print(f"You have worked for {numberHourWork} hours and your wage per hour is ${wagePerHour}")
 
 if 40 < numberHourWork <= 168:
-    # calculates the wage after 40 hours
+    # getting the amount of hours after the first 40 hours
     overtimeHourWork = numberHourWork - 40
+    # getting the overtime wage rate
     overtimeWagePerHour = wagePerHour * 1.5
+    # calculating the overtime pay
     totalOvertimeWage = overtimeHourWork * overtimeWagePerHour
     print(f"You earned ${totalOvertimeWage} for your overtime")
+
     # only the first 40 hours has not been calculated
     totalWage = ( 40 * wagePerHour ) + totalOvertimeWage
 elif numberHourWork > 168:
@@ -18,3 +21,12 @@ else:
 
 print(f"You earned ${totalWage}")
 # print(f"\nID : , Initials: ")
+
+# questions to ask
+# 1. i have done the pseudocode and i already have done the code, the next thing to do now is the test plan,
+    # when i ran through the test plan, i have not accounted the pseudocode to include where the input is in negative number
+    # should i just submit my work with test plan that includes what i should code in to fix the error
+    # or
+    # i should fix the pseudocode, the code so i dont need to include the explainations in the test plan?
+# - the fix is TC-003 and TC-004 to include try catch input validation test, same with scenario 2
+# depending on the teacher answer, i should be able to submit this by week 4.
--- monthly_budget_tracker.py
+++ monthly_budget_tracker.py
@@ -16,10 +16,39 @@
     # a.	INPUT expenseCategory
     # b.	totalExpenseCategory = totalExpenseCategory + expenseCategory
     # c.	INPUT expenseAmount
-    # d.	totalExpenseAmount =  totalExxpenseAmount + expenseAmount
+    # d.	totalExpenseAmount =  totalExpenseAmount + expenseAmount
     # e.	OUTPUT “Do you want to continue (Y/N)?”
     # f.	INPUT continueButton
 # 13.	ENDWHILE
 # 14.	remainingBalance = wagePerMonth - totalExpenseAmount
 # 15.	OUTPUT wagePerMonth,  totalExpenseAmount, remainingBalance
 # 16.	END
+
+wagePerMonth = float(input("Enter your wage per month: "))
+
+# implement a try catch input validation test here
+# 4.	WHILE wagePerMonth < 0 OR wagePerMonth == isNumeric
+    # a.	OUTPUT “Your wage is in the negatives or is not numeric. Please re-enter your wage”
+    # b.	INPUT wagePerMonth
+
+expenseCategory = input("Enter your expense category: ")
+expenseCategory = expenseCategory.upper()
+totalExpenseAmount = float(input("Enter your expense amount: "))
+
+continueButton = input("Would you like to continue? (y/n): ")
+
+while continueButton == "y":
+    expenseCategory = input("Enter your expense category: ")
+    expenseCategory = expenseCategory.upper()
+
+    expenseAmount = float(input("Enter your expense amount: "))
+    totalExpenseAmount = totalExpenseAmount + expenseAmount
+
+    continueButton = input("Would you like to continue? (y/n): ")
+
+remainingBalance = wagePerMonth - totalExpenseAmount
+print(f"You have earned ${wagePerMonth} this month. Your total expense is ${totalExpenseAmount}\n"
+      f"Your remaining balance is ${remainingBalance}.")
+
+# print(f"{expenseCategory} expense is ${totalExpenseAmount}")
+# print(f"\nID : , Initials: ")
