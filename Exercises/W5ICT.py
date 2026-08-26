66f8071b98a3d7f55be2a2fe7927667e9199da66
30148167 <30148167@tafe.wa.edu.au>
2026-08-26 15:10:46 +0800

    Adding W5 lab exercises

---
+++ Exercises/W5ICT.py
@@ -0,0 +1,22 @@
+# x=5
+
+# print(x>3)
+# print(x<2)
+# print(x>3 and x<2)
+# print(x>3 or x<2)
+# print(not x>3)
+#
+# True
+# False
+# False
+# False
+# True
+
+# for x in range(1, 6):
+#     print(x, "squared is", x * x)
+
+# import W5importingmodules
+# W5importingmodules.greeting("Nuha")
+
+
+
